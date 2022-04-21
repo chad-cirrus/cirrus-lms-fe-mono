780768cbaced8c94e150ce19cd70f4465636e3a4
dharms40au <92381262+dharms40au@users.noreply.github.com>
2022-04-21 14:29:11 -0500

    bug fixes (#40)
    Co-authored-by: Dave Harms <daveharmswebdev@gmail.com>

--- libs/ui/src/lib/lesson-content-item/lesson-content-item.component.ts
+++ libs/ui/src/lib/lesson-content-item/lesson-content-item.component.ts
@@ -58,23 +58,23 @@ export class LessonContentItemComponent {
   private mapContentTypeToIcon(contentType: number): string {
     switch (contentType) {
       case 0:
-        return 'images/svg/video_play.svg';
+        return 'courses/images/svg/video_play.svg';
       case 4:
-        return 'images/svg/document_button.svg';
+        return 'courses/images/svg/document_button.svg';
       case 5:
-        return 'images/svg/quiz_lc_icon.svg';
+        return 'courses/images/svg/quiz_lc_icon.svg';
       case 6:
-        return 'images/svg/document_button.svg';
+        return 'courses/images/svg/document_button.svg';
       case 8:
-        return 'images/svg/rich_text_icon.svg';
+        return 'courses/images/svg/rich_text_icon.svg';
       case 9:
-        return 'images/svg/assessment_lc_icon.svg';
+        return 'courses/images/svg/assessment_lc_icon.svg';
       case 10:
-        return 'images/svg/assessment_lc_icon.svg';
+        return 'courses/images/svg/assessment_lc_icon.svg';
       case 11:
-        return 'images/svg/image_lc_icon.svg';
+        return 'courses/images/svg/image_lc_icon.svg';
       default:
-        return 'images/svg/document_button.svg';
+        return 'courses/images/svg/document_button.svg';
     }
   }
 }
