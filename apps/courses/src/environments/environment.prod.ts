0174fc947c420106b455beb1b574d4fb7773cbfd
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2022-06-27 14:09:38 -0400

    added missinng env vars (#158)
    Co-authored-by: Andrew Lastrapes <andrewlastrapes@Andrews-MacBook-Pro.local>

--- apps/courses/src/environments/environment.prod.ts
+++ apps/courses/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://cirrus-lms-ppl-api-stage.herokuapp.com',
+  defaultMobile: "https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-hero-mobile.jpg",
+  defaultDesktop: "https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-hero-desktop.jpg"
 };
