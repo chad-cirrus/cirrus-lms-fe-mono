a5833456b5c2784e6cd7de8dbd09eaebbc9d7e15
Michael Yockey <myockey@cirrusaircraft.com>
2022-08-25 10:40:07 -0400

    chore: Preserve build process results

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+build_result=0
+storybook_result=0
+
 cp apps/courses/src/environments/environment.ts.example apps/courses/src/environments/environment.ts
 yarn run build-projects
+build_result=$?
 if [[ $CREATE_STORYBOOK == "true" ]];
   then
     echo "building storybook"
     yarn run build-storybook;
+    storybook_result=$?
   else
     echo "skip creating storybook";
 fi
-# yarn run build-projects
+
+exit $((build_result + storybook_result))
