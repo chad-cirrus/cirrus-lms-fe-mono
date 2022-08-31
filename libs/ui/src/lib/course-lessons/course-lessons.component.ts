2a1eb0203f305d078a2e55836cf0fdda620af0e6
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2022-08-31 13:24:36 -0500

    merge conflicts (#248)
    * merge conflicts
    
    * remove console log
    
    Co-authored-by: Andrew Lastrapes <andrewlastrapes@Andrews-MBP.lan>

--- libs/ui/src/lib/course-lessons/course-lessons.component.ts
+++ libs/ui/src/lib/course-lessons/course-lessons.component.ts
@@ -1,10 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import {
-
   FilterMenuSection,
   ICourseOverviewLesson,
-
   ICourseOverviewStage,
 } from '@cirrus/models';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
@@ -18,22 +16,18 @@ import { filter, map, startWith, tap } from 'rxjs/operators';
 export class CourseLessonsComponent implements OnInit {
   private _stages!: ICourseOverviewStage[];
   stagesSubject = new BehaviorSubject<ICourseOverviewStage[]>([]);
-
   lessonsSubject = new BehaviorSubject<ICourseOverviewLesson[]>([]);
   filteredStages$!: Observable<ICourseOverviewStage[]>;
   filteredLessons$!: Observable<ICourseOverviewLesson[]>;
 
-
   @Input()
   set stages(value: ICourseOverviewStage[]) {
     this._stages = value;
-
     const combineTotalLessonsCount = value.map(stage => stage.lessons);
     const combine = this.combineLessons(combineTotalLessonsCount);
     this.lessonsOverallCount = combine.length;
     this.stagesSubject.next(value);
     this.lessonsSubject.next(combine);
-
   }
 
   get stages() {
@@ -44,96 +38,6 @@ export class CourseLessonsComponent implements OnInit {
   lessonsOverallCount!: number;
 
   @Output() navigate = new EventEmitter<number>();
-  showPanel$!: Observable<boolean>;
-
-  filterForm = new FormGroup({
-    filterText: new FormControl(''),
-    filterCheckbox: new FormArray([]),
-  });
-
-  changes!: Filter;
-
-  filterMenuSections: FilterMenuSection[] = [
-    { title: 'Status', items: ['completed', 'in_progress', 'not_started'] },
-    { title: 'Stages', items: ['Stage 1', 'Stage 2', 'Stage 3', 'Stage 4'] },
-  ];
-
-  get checkboxArray() {
-    return this.filterForm.get('filterCheckbox') as FormArray;
-  }
-
-  ngOnInit() {
-    this.filteredStages$ = combineLatest([
-      this.filterForm.valueChanges.pipe(
-        startWith({ filterText: '', filterCheckbox: [] })
-      ),
-      this.stagesSubject,
-    ]).pipe(
-      map(([changes, stages]) => {
-        this.changes = changes;
-        return stages.filter(stage => this.filteredStages(changes, stage));
-      }),
-      map(stage =>
-        stage.filter(stage => this.filteredBoxes(this.changes, stage))
-      ),
-      map(stage =>
-        stage.filter(stage => this.filteredSearchInput(this.changes, stage))
-      ),
-      tap(stage => {
-        const combineDisplayedLessons = stage.map(s => s.lessons);
-        this.lessonsDisplayedCount = this.findLessonCount(
-          combineDisplayedLessons
-        );
-      })
-    );
-  }
-
-  findLessonCount(lessons: any) {
-    return [].concat(...Object.keys(lessons).map(k => lessons[k])).length;
-  }
-
-  filteredStages(changes: Filter, stage: ICourseOverviewStage) {
-    if (!changes.filterCheckbox.length) {
-      return true;
-    }
-    if (changes.filterCheckbox.includes(`Stage ${stage.order + 1}`)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  filteredBoxes(changes: Filter, stage: ICourseOverviewStage) {
-    if (!changes.filterCheckbox.length) {
-      return true;
-    }
-    if (this.isProgressFiltered(changes)) {
-      return changes.filterCheckbox.includes(stage.progress.status);
-    } else {
-      return true;
-    }
-  }
-
-  filteredSearchInput(changes: Filter, stage: ICourseOverviewStage) {
-    if (!changes.filterText) {
-      return true;
-    } else {
-      return stage.title.includes(changes.filterText);
-    }
-  }
-
-  isProgressFiltered(changes: Filter) {
-    return (
-      changes.filterCheckbox.includes('not_started') ||
-      changes.filterCheckbox.includes('completed') ||
-      changes.filterCheckbox.includes('in_progress')
-    );
-  }
-
-  removeTag(tag: string) {
-    const index = this.checkboxArray.value.indexOf(tag);
-    this.checkboxArray.removeAt(index);
-  }
 
   filterForm = new FormGroup({
     filterText: new FormControl(null),
@@ -188,7 +92,6 @@ export class CourseLessonsComponent implements OnInit {
           ? []
           : lessons.filter(l => {
               const format = l.progress.status.replace('_', ' ');
-              console.log('l', l);
               return (
                 l.title
                   .toLowerCase()
