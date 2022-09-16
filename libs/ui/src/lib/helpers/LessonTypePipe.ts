3f64469fbbfc9619cb0fa40817af52aada52a10c
Corey Walter <info@coreycodes.com>
2022-09-16 10:11:22 -0400

    adds sim lesson label to lesson hero
    adds sim lesson as lesson type for stages overlay
    
    resolves ADO-28293

--- libs/ui/src/lib/helpers/LessonTypePipe.ts
+++ libs/ui/src/lib/helpers/LessonTypePipe.ts
@@ -3,10 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'lessonTypePipe' })
 export class LessonTypePipe implements PipeTransform {
   transform(lessonType: number): string {
+    console.log('lesson type', lessonType)
     const dictionary: { [key: number]: string } = {
       [0]: 'Self Study',
       [1]: 'Ground',
       [2]: 'Flight',
+      [3]: 'Simulator'
     };
 
     return dictionary[lessonType];
