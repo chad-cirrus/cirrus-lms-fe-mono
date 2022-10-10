03fb8d34e0d7017286613eed47934b257d689135
dharms40au <92381262+dharms40au@users.noreply.github.com>
2022-10-10 16:23:28 -0500

    refactor course in progress cta to route to next lesson (#327)
    Co-authored-by: Dave Harms <daveharmswebdev@gmail.com>

--- apps/recent-activity/src/app/models/IRecentActivity.ts
+++ apps/recent-activity/src/app/models/IRecentActivity.ts
@@ -47,7 +47,11 @@ export interface InProgressCourses {
   lessons_completed: number;
   lessons_total: number;
   name: string;
-  thumbnail_image_url: string;
+  thumbnail_image_url: string | null;
+  next_lesson: {
+    stage_id: number;
+    lesson_id: number;
+  };
 }
 
 export const initialRecentActivity: IRecentActivity = {
