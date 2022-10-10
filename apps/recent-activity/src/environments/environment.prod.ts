ed0071ba8cf62f669c1429f0914fcc658fbac7c0
dharms40au <92381262+dharms40au@users.noreply.github.com>
2022-10-10 18:58:29 -0500

    Bug/thumbnail 29143 download link 29147 (#330)
    * refactor download cert and added default thumbnail image
    
    * refactor environment.prod.ts
    
    Co-authored-by: Dave Harms <daveharmswebdev@gmail.com>

--- apps/recent-activity/src/environments/environment.prod.ts
+++ apps/recent-activity/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://cirrus-lms-ppl-api-stage.herokuapp.com',
+  defaultLessonThumbnail:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-thumbnail.jpg',
 };
