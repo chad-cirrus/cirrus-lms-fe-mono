6c1d7c04d0296f8e6a61e12b86e866ce26e474fb
dharms40au <92381262+dharms40au@users.noreply.github.com>
2022-11-02 14:24:32 -0500

    refactor hove and loading state (#391)
    Co-authored-by: Dave Harms <daveharmswebdev@gmail.com>

--- libs/ui/src/lib/helpers/LessonTypePipe.ts
+++ libs/ui/src/lib/helpers/LessonTypePipe.ts
@@ -3,12 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'lessonTypePipe' })
 export class LessonTypePipe implements PipeTransform {
   transform(lessonType: number): string {
-    console.log('lesson type', lessonType)
     const dictionary: { [key: number]: string } = {
       [0]: 'Self Study',
       [1]: 'Ground',
       [2]: 'Flight',
-      [3]: 'Simulator'
+      [3]: 'Simulator',
     };
 
     return dictionary[lessonType];
