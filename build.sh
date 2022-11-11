77f21dd468db9009dffe5e49eccb9d9f59c7678c
Michael Yockey <myockey@cirrusaircraft.com>
2022-11-11 10:06:11 -0500

    chore: Restore example file copy in build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ storybook_result=0
 
 environment=${NX_ENV=production}
 
+cp apps/courses/src/environments/environment.ts.example apps/courses/src/environments/environment.ts
 yarn nx run-many --target=build --projects=courses,atc,recent-activity --configuration="$environment"
 build_result=$?
 if [[ $CREATE_STORYBOOK == "true" ]];
