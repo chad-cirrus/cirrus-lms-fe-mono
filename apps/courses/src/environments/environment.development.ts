d675a17feeb91d481ccd53e58caa4cd56ab6dd35
Michael Yockey <myockey@cirrusaircraft.com>
2023-01-23 17:03:40 -0500

    chore: Make NX_* environment variables available to Webpack

---
+++ apps/courses/src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+ export const environment = {
+  production: true,
+  project: 'courses',
+  baseUrl: process.env['NX_BASE_URL'],
+  profile: 'https://login.cirrusapproach.com/s/profile/',
+  defaultLessonThumbnail:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-thumbnail.jpg',
+  defaultMobileLesson:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-hero-mobile.jpg',
+  defaultDesktopLesson:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-hero-desktop.jpg',
+  defaultMobileCourse:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-course-hero-sm.jpg',
+  defaultDesktopCourse:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-course-hero-lg.jpg',
+};
