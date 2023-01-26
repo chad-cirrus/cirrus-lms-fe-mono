642f7f1cdd4ed99020beff5ca94550e7617c224c
Michael Yockey <myockey@cirrusaircraft.com>
2023-01-26 14:54:12 -0500

    chore: Support environment variables at build time

---
+++ apps/recent-activity/src/environments/environment.template.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  project: 'recent-activity',
+  baseUrl: process.env['NX_BASE_URL'],
+  profile:
+    'https://cirrusaircraft--cirfullsb.sandbox.my.site.com/approachsso/s/profile/',
+  defaultThumbnailCourse:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-course-thumbnail.jpg',
+  defaultLessonThumbnail:
+    'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-thumbnail.jpg',
+};
