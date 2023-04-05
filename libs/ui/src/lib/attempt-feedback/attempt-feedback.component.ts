74192dc29413e59fafdce0e1985634333139d861
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2023-04-05 13:46:34 -0400

    missed task nav not navigating to attempt feedback (#588)
    Co-authored-by: Andrew Lastrapes <andrewlastrapes@Andrews-MacBook-Pro.local>

--- libs/ui/src/lib/attempt-feedback/attempt-feedback.component.ts
+++ libs/ui/src/lib/attempt-feedback/attempt-feedback.component.ts
@@ -1,21 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Attempt } from '@cirrus/models';
 
 @Component({
   selector: 'cirrus-attempt-feedback',
   templateUrl: './attempt-feedback.component.html',
-  styleUrls: ['./attempt-feedback.component.scss']
+  styleUrls: ['./attempt-feedback.component.scss'],
 })
 export class AttemptFeedbackComponent {
-
   @Input() attempt!: Attempt;
   @Input() attempt_index!: number;
   @Output() emitBack = new EventEmitter();
 
-
-
   back() {
-    this.emitBack.emit()
+    this.emitBack.emit();
   }
-
 }
