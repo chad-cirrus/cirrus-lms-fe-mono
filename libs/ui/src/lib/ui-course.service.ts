627fc4ca64bed95895c780d02bea6ef59a0856d2
Michael Yockey <myockey@cirrusaircraft.com>
2023-08-31 17:44:14 -0400

    fix: Manage shopping cart state correctly when logged out
    Fixed two issues for adding courses to the shopping cart while logged out.
    - Default null order object caused an invalid course to be added to shopping cart
    - Adding a course while logged out caused existing courses to be cleared from shopping cart after login

--- libs/ui/src/lib/ui-course.service.ts
+++ libs/ui/src/lib/ui-course.service.ts
@@ -1,17 +1,20 @@
-import { Inject, Injectable } from '@angular/core';
-import { CoursePreviewVideoPlayerComponent } from './course-preview-video-player/course-preview-video-player.component';
-import { IContent, ICourseOverview, IOrder } from '@cirrus/models';
-import { MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { IContent, ICourseOverview, IOrder } from '@cirrus/models';
 import { Observable, of } from 'rxjs';
+import { CoursePreviewVideoPlayerComponent } from './course-preview-video-player/course-preview-video-player.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UiCourseService {
   private readonly environment: Record<string, unknown>;
-  constructor(private dialog: MatDialog, private http: HttpClient,
-    @Inject('environment') environment: Record<string, unknown>) {
+  constructor(
+    private dialog: MatDialog,
+    private http: HttpClient,
+    @Inject('environment') environment: Record<string, unknown>,
+  ) {
     this.environment = environment;
   }
 
@@ -25,18 +28,12 @@ export class UiCourseService {
   }
 
   freeCourseEnroll(course: ICourseOverview): Observable<any> {
-    return this.http.post(
-      `${this.environment.baseUrl}/api/v4/user_courses/`,
-      { course_id: course.id }
-    );
+    return this.http.post(`${this.environment.baseUrl}/api/v4/user_courses/`, { course_id: course.id });
   }
 
   courseReEnroll(payload: ReEnrollPayload) {
     const { course_id, user_id } = payload;
-    return this.http.post(
-      `${this.environment.baseUrl}/api/v3/courses/reenroll/${course_id}/${user_id}`,
-      {}
-    );
+    return this.http.post(`${this.environment.baseUrl}/api/v3/courses/reenroll/${course_id}/${user_id}`, {});
   }
 
   courseEnroll(course: ICourseOverview, order: IOrder | null): Observable<any> {
@@ -47,27 +44,24 @@ export class UiCourseService {
             product_id: course.id,
             product: {
               list_price: course.list_price?.toString(),
-            }
-          }
+            },
+          },
         ],
       },
     };
 
     if (order && order.order_line_items.length > 0) {
       const previousCartItems = [...order.order_line_items];
-      const courseExistsInCart = previousCartItems.filter((item) => item.product_id === course.id).length !== 0;
+      const courseExistsInCart = previousCartItems.filter(item => item.product_id === course.id).length !== 0;
 
       if (courseExistsInCart) {
-        return of(true)
+        return of(true);
       }
 
       formatOrder.order.order_line_items.push(...previousCartItems);
     }
 
-    return this.http.post(
-      `${this.environment.baseUrl}/api/v3/orders/update-cart`,
-      formatOrder
-    );
+    return this.http.post(`${this.environment.baseUrl}/api/v3/orders/update-cart`, formatOrder);
   }
 
   courseEnrollForUnauth(course: ICourseOverview): Observable<any> {
@@ -79,23 +73,23 @@ export class UiCourseService {
             product: {
               list_price: course.list_price?.toString(),
               title: course.title,
-              thumbnail_image_url: course.thumbnail_image_url
+              thumbnail_image_url: course.thumbnail_image_url,
             },
           },
-        ]
-      }
-    }
+        ],
+      },
+    };
     const exisitingCourses = JSON.parse(<string>localStorage.getItem('checkout-state'));
 
     if (exisitingCourses !== null) {
       const existingOrderLineItems = [...exisitingCourses.order.order_line_items];
 
       if (existingOrderLineItems.length > 0) {
-        const courseExistsInCart = existingOrderLineItems.filter((item) => item.product_id === course.id).length !== 0;
+        const courseExistsInCart = existingOrderLineItems.filter(item => item.product_id === course.id).length !== 0;
         if (courseExistsInCart) {
           return of(true);
         }
-        newOrder.order.order_line_items.push(...existingOrderLineItems)
+        newOrder.order.order_line_items.push(...existingOrderLineItems);
       }
     }
 
