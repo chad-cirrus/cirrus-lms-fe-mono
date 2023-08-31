b24dbb4bfd0e7b0a04bf653878555d6033b2a55a
Michael Yockey <myockey@cirrusaircraft.com>
2023-08-31 17:44:14 -0400

    fix: Manage shopping cart state correctly when logged out
    Fixed two issues for adding courses to the shopping cart while logged out.
    - Default null order object caused an invalid course to be added to shopping cart
    - Adding a course while logged out caused existing courses to be cleared from shopping cart after login

--- apps/courses/src/app/store/actions/orders.actions.ts
+++ apps/courses/src/app/store/actions/orders.actions.ts
@@ -1,17 +1,8 @@
-import { ILesson, IOrder } from '@cirrus/models';
+import { IOrder } from '@cirrus/models';
 import { createAction, props } from '@ngrx/store';
 
-export const fetchOrders = createAction(
-  'fetch orders',
-  props<{ order: IOrder }>()
-);
+export const fetchOrders = createAction('fetch orders');
 
-export const fetchOrdersSuccess = createAction(
-  'fetch orders success',
-  props<{ order: IOrder }>()
-);
+export const fetchOrdersSuccess = createAction('fetch orders success', props<{ order: IOrder }>());
 
-export const fetchOrdersFailure = createAction(
-  'fetch orders failure',
-  props<{ error: any }>()
-);
+export const fetchOrdersFailure = createAction('fetch orders failure', props<{ error: any }>());
