24d05bf47eb90583c206289090bb12992ecbe798
grantfoglecirrus <138698426+grantfoglecirrus@users.noreply.github.com>
2023-09-13 08:00:30 -0600

    adding updated edit profile and and flight deck link (#754)

--- apps/courses/src/environments/environment.template.ts
+++ apps/courses/src/environments/environment.template.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true,
   project: 'courses',
   baseUrl: process.env['NX_BASE_URL'],
-  profile:
-    'https://env-ciam-cirrus-aircraft-multisite.pantheonsite.io/account/profile/',
+  profile: 'https://cirrusaircraft.com?app_name=Salesforce&option=oauthredirect&redirect_url=https%3A%2f%2fcirrusaircraft.com%2faccount%2fprofile',
   defaultLessonThumbnail:
     'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-lesson-thumbnail.jpg',
   defaultMobileLesson:
--- apps/recent-activity/src/environments/environment.prod.ts
+++ apps/recent-activity/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   project: 'recent-activity',
   baseUrl: 'https://learning.cirrusapproach.com',
-  profile: 'https://cirrusaircraft.com/account/profile/',
+  profile: 'https://cirrusaircraft.com?app_name=Salesforce&option=oauthredirect&redirect_url=https%3A%2f%2fcirrusaircraft.com%2faccount%2fprofile',
   defaultThumbnailCourse:
     'https://cirrusapproachherokuprod.blob.core.windows.net/cirruslmsherokudevcontainer/content-items/images/default-course-thumbnail.jpg',
   defaultLessonThumbnail:
