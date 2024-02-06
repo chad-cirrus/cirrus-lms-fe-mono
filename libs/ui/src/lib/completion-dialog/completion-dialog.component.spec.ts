91abd66598df6454159bef77e77cc5f55adfb390
Chad Kranz <chad.kranz@cirrusaircraft.com>
2024-02-06 18:49:22 -0500

    fixing broken unit test

--- libs/ui/src/lib/completion-dialog/completion-dialog.component.spec.ts
+++ libs/ui/src/lib/completion-dialog/completion-dialog.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompletionDialogComponent } from './completion-dialog.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { UiDownloadService } from '../course-completion/ui-download.service';
+import { ICourseOverview } from '@cirrus/models';
+import { of } from 'rxjs';
 
 describe('CompletionDialogComponent', () => {
   let component: CompletionDialogComponent;
@@ -10,23 +13,32 @@ describe('CompletionDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompletionDialogComponent ],
+      declarations: [CompletionDialogComponent],
       imports: [MatMenuModule, MatDialogModule],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: {} },
         { provide: MatDialogRef, useValue: {} },
-      ]
-    })
-    .compileComponents();
+        { provide: UiDownloadService, useValue: { getCourse: () => jest.fn().mockReturnValue(of({}))} }, // Provide a mock implementation for getCourse
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CompletionDialogComponent);
     component = fixture.componentInstance;
+    component.data = {
+      lesson: 'lesson',
+      course: {} as ICourseOverview,
+      stageId: 1,
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    // Provide a mock implementation for uiDownloadService
+    const uiDownloadService = TestBed.inject(UiDownloadService);
+    uiDownloadService.getCourse = ;
     expect(component).toBeTruthy();
   });
 });
+
