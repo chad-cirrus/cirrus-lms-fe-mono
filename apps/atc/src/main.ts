85c22084a36d5a45f2665c54c9b86c94c45acb19
Michael Yockey <myockey@cirrusaircraft.com>
2024-03-13 15:59:23 -0400

    chore: Remove debug logging

--- apps/atc/src/main.ts
+++ apps/atc/src/main.ts
@@ -15,14 +15,11 @@ if (process.env.CREATE_STORYBOOK) {
 }
 
 app.get('/:app//?*', (req, res) => {
-  console.log('agnostic middleware');
   const { params: { app } } = req;
-  console.log('app', app);
   res.sendFile(path.join(__dirname, '..', app, 'index.html'));
 });
 
 app.get('/recent-activity/?*', (req, res) => {
-  console.log('recent-activity middleware');
   res.sendFile(path.join(__dirname, '..', 'recent-activity', 'index.html'));
 });
 
