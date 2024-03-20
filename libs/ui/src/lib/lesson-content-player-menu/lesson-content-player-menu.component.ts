f84a68a30cbf10adf90d957e8f9d14322aec6b7d
balves <balves@lopes.com.br>
2024-03-20 11:02:31 -0300

    42403 fix content player navigation

--- libs/ui/src/lib/lesson-content-player-menu/lesson-content-player-menu.component.ts
+++ libs/ui/src/lib/lesson-content-player-menu/lesson-content-player-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { IContentPlayerMenuItem, PROGRESS_STATUS } from '@cirrus/models';
 import { progressIconMapper } from '../helpers/ProgressIconMapper';
 
@@ -7,12 +7,13 @@ import { progressIconMapper } from '../helpers/ProgressIconMapper';
   templateUrl: './lesson-content-player-menu.component.html',
   styleUrls: ['./lesson-content-player-menu.component.scss'],
 })
-export class LessonContentPlayerMenuComponent {
+export class LessonContentPlayerMenuComponent implements OnInit {
   private _menuItems: IContentPlayerMenuItem[] = [];
 
   leftChevron = 'courses/images/svg/desktop-sn-l-chevron.svg';
   downChevron = 'courses/images/svg/down-chevron.svg';
   menuIcon = 'courses/images/svg/lesson-icon-heavy.svg';
+  isMobile = false;
   @Output() closeMenu = new EventEmitter<void>();
   @Output() playContent = new EventEmitter<number>();
 
@@ -31,11 +32,25 @@ export class LessonContentPlayerMenuComponent {
     return this._menuItems;
   }
 
+  ngOnInit(): void {
+    //Check device resolution
+    window.innerWidth <= 959 ? this.isMobile = true : this.isMobile = false
+    window.onresize = () => this.isMobile = window.innerWidth <= 959;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    event <= 959 ? this.isMobile = true : this.isMobile = false;
+  }
+
   close() {
     this.closeMenu.emit();
   }
 
   emitPlayContent(id: number) {
     this.playContent.emit(id);
+    if (this.isMobile) {
+      this.close();
+    }
   }
 }
