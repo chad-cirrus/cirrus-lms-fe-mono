29ff6c3dd8e39146f6a905fcc5ec0984e5186b5c
Chris Steurer <chris.steurer@fortyau.com>
2024-05-14 12:03:39 -0500

    Adding base component files

---
+++ apps/ctc-admin/remotes/header/src/app/remote-entry/components/user-profile-menu/user-profile-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ctc-user-profile-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-profile-menu.component.html',
+  styleUrl: './user-profile-menu.component.scss',
+})
+export class UserProfileMenuComponent {}
