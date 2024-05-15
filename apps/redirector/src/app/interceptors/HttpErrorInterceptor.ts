5e74b873362a59b8aa926f893c7db26eea5e0c8e
balves <balves@lopes.com.br>
2024-05-15 12:09:19 -0300

    Removing unnecessary code

--- apps/redirector/src/app/interceptors/HttpErrorInterceptor.ts
+++ apps/redirector/src/app/interceptors/HttpErrorInterceptor.ts
@@ -13,15 +13,14 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
-  // constructor(private errorService: ErrorService) {}
   constructor() {}
+  
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        // this.errorService.errorSubject.next(error.error);
         return throwError(error.error);
       })
     );
