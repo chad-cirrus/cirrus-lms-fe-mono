1b552fb7c321555c5c4ffe9cb5e6f9db14e535d0
balves <balves@lopes.com.br>
2024-05-15 04:00:12 -0300

    Redirector app interceptors

---
+++ apps/redirector/src/app/interceptors/HttpErrorInterceptor.ts
@@ -0,0 +1,30 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, throwError } from 'rxjs';
+
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  // constructor(private errorService: ErrorService) {}
+  constructor() {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log("interceptError");
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // this.errorService.errorSubject.next(error.error);
+        return throwError(error.error);
+      })
+    );
+  }
+}
