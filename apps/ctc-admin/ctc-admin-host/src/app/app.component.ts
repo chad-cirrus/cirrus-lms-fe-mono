cc204aec8243153a2017795623ced5f04c9e384b
chad-cirrus <145160266+chad-cirrus@users.noreply.github.com>
2024-05-20 15:06:14 -0500

    ctc admin user guard, plus wire up some header data fields (#948)
    * ctc admin user guard, plus wire up some header data fields
    
    * fix: failing unit tests
    
    * fix: failing jest tests due to incorrect local storage implementation
    
    * fix: remove unnecessary code, remove PII

--- apps/ctc-admin/ctc-admin-host/src/app/app.component.ts
+++ apps/ctc-admin/ctc-admin-host/src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICirrusUser } from '@cirrus/models';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ctc-admin';
+  cirrusUser = JSON.parse(<string>localStorage.getItem('cirrus-user')) as ICirrusUser;
+
+  ngOnInit(): void {
+
+    if(!this.userIsCTCAdmin()) {
+      window.location.href = '/learning-catalog';
+    }
+  }
+  userIsCTCAdmin(): boolean {
+    return this.cirrusUser && this.cirrusUser.ctc_admin;
+  }
 }
--- apps/ctc-admin/remotes/header/src/app/remote-entry/components/header.component.ts
+++ apps/ctc-admin/remotes/header/src/app/remote-entry/components/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileMenuComponent } from './user-profile-menu/user-profile-menu.component';
+import { ICirrusUser } from '@cirrus/models';
 
 @Component({
   selector: 'ctc-admin-header',
@@ -9,4 +10,24 @@ import { UserProfileMenuComponent } from './user-profile-menu/user-profile-menu.
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit{
+  cirrusUser = JSON.parse(<string>localStorage.getItem('cirrus-user')) as ICirrusUser;
+
+  ngOnInit(): void {
+
+    if(!this.userIsCTCAdmin()) {
+      window.location.href = '/learning-catalog';
+    }
+  }
+  userIsCTCAdmin(): boolean {
+    return this.cirrusUser && this.cirrusUser.ctc_admin;
+  }
+
+  getUserInitials(): string {
+    return this.cirrusUser.firstname[0] + this.cirrusUser.lastname[0];
+  }
+
+  getDisplayName(): string {
+    return this.cirrusUser.firstname + ' ' + this.cirrusUser.lastname;
+  }
+}
